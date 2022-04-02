c9c125de30c40365f6bebf51a5e674e538789f80
Ge Liu <gl69821@gmail.com>
2022-04-02 16:18:41 -0700

    form

--- js/formValidation.js
+++ js/formValidation.js
@@ -1,30 +1,35 @@
-<script type = "text/javascript">
+document.myForm = undefined;
 
-    validate()
+function validate()
+{
+    var username=  /^[a-zA-Z][a-zA-Z0-9]{3,20}/;
+
+    if(!document.myForm.username.value.match(username))
     {
-        var user=  /^[a-zA-Z][a-zA-Z0-9]{3,20}/;
-        if(!document.myForm.userName.value.match(user))
-    {
-        alert('Please enter correct Username format')
+        alert('Please enter correct Username format');
         return false;
     }
-        var pass=  /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
-        if(!document.myForm.password.value.match(pass))
+
+    var pass=  /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
+
+    if(!document.myForm.pass.value.match(pass))
     {
-        alert('Please enter correct password format')
+        alert('Please enter correct password format');
         return false;
     }
-        if(document.myForm.confirmPassword.value != document.myForm.password.value)
+    if(document.myForm.conpass.value != document.myForm.password.value)
     {
-        alert('Your password do not matches')
+        alert('Your password do not matches');
         return false;
     }
-        var emailID = document.myForm.email.value;
-        atpos = emailID.indexOf("@");
-        dotpos = emailID.lastIndexOf(".");
-        if (atpos < 1 || ( dotpos - atpos < 2 )) {
+    var email = document.myForm.email.value;
+
+    atpos = email.indexOf("@");
+    dotpos = email.lastIndexOf(".");
+
+    if (atpos < 1 || ( dotpos - atpos < 2 )) {
         alert("Please enter correct email ID");
         return false;
-        }
     }
-</script>
+
+}
