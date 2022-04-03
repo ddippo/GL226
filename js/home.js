6dc91da19618008b22f41fcb880e5b872112b34f
Ge Liu <gl69821@gmail.com>
2022-04-02 23:02:34 -0700

    form

--- js/home.js
+++ js/home.js
@@ -3,19 +3,17 @@ function loadDoc() {
     let ajaxCall = new XMLHttpRequest();
     ajaxCall.onload = function () {
         console.log('sucess...');
-        if (this.readyState == 4 && this.status == 200) {
-            let x = JSON.parse(this.responseText);
-            num = x.length;
-            document.getElementById("view").innerHTML =`${num} Photos`;
-            x.forEach((obj) => {
-                document.getElementById(
-                    "photo"
-                ).innerHTML += `<div id=${obj.id} class="gallery" onclick="fade(${obj.id})">
-                        <img src=${obj.url} width="400" height="300"/>
-                        <div class="de">${obj.title}</div>
-                     </div>`;
+        let x = JSON.parse(this.responseText);
+        num = x.length;
+        document.getElementById("view").innerHTML =`${num} Photos`;
+        x.forEach((obj) => {
+            document.getElementById(
+                "photo"
+            ).innerHTML += `<div id=${obj.id} class="gallery" onclick="fade(${obj.id})">
+                <img src=${obj.url} width="400" height="300"/>
+                <div class="de">${obj.title}</div>
+                </div>`;
             });
-        }
     };
     ajaxCall.open("GET", "https://jsonplaceholder.typicode.com/albums/2/photos", true);
     ajaxCall.send();
