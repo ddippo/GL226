a7e18f9d84dd9c06af0894d3ea29f6543e79731c
Ge Liu <gl69821@gmail.com>
2022-04-02 20:05:16 -0700

    form

--- js/formValidation.js
+++ js/formValidation.js
@@ -7,11 +7,11 @@ function validate() {
 
     let pass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
     if(!document.reg.pass.value.match(pass)) {
-        alert('Please enter correct password format')
+        alert('Please enter the correct password format')
         return false;
     }
     if(document.reg.conpass.value != document.reg.pass.value) {
-        alert('Your password do not matches')
+        alert('Your passwords do not match')
         return false;
     }
 } 
